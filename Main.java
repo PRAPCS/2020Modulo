a3c0cf9e6368945f511951da65256e7a90c032bf
Mr. Burger <bburger@d155.org>
2020-09-21 19:19:59 +0000

    example

--- Main.java
+++ Main.java
@@ -8,6 +8,11 @@ class Main {
    System.out.println(123%10); 
    System.out.println(123456%10); //Returns last digit in number
    System.out.println(123/10); //Removes the last digit
+  System.out.println(num1+" + "+num2+" = "+(num1+num2));
 
+  123% 10 = 3
+  123/10 = 12
+  12%10 = 2
+  12/10 = 1
   }
 }
