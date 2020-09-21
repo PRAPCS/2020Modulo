bf7f3c541244ed05ed65ab50530369112d435be6
Mr. Burger <bburger@d155.org>
2020-09-21 18:19:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    
+   System.out.println(10%2); 
+   System.out.println(9%3); 
+   System.out.println(11%5); 
+   System.out.println(5%8); 
+   System.out.println(123%10); 
+   System.out.println(123/10); 
+
+  }
+}
