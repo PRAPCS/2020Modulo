522b6ecae969a70846eb4929adeaf84d3c419543
Mr. Burger <bburger@d155.org>
2020-09-21 19:02:18 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@ class Main {
   public static void main(String[] args) {
     
    System.out.println(10%2); 
-   System.out.println(9%3); 
+   System.out.println(9%2);
    System.out.println(11%5); 
    System.out.println(5%8); 
    System.out.println(123%10); 
-   System.out.println(123/10); 
+   System.out.println(123456%10); //Returns last digit in number
+   System.out.println(123/10); //Removes the last digit
 
   }
 }
